9971a1fd9c73bb58735626162826f665931135b4
lmc <101943576+april808@users.noreply.github.com>
2023-06-23 19:13:17 +0800

    vue改寫商品卡片

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue-work
 個專改寫小作業
+
+- 個專: [赤心Pure_Dessert](https://ioop987.github.io/pure-dessert/news.html)
+  | [檔案下載](https://github.com/ioop987/pure-dessert)
---
+++ vue.js
@@ -0,0 +1,63 @@
+Vue.createApp({
+  data() {
+    return {
+      productList: [
+        {
+          sort: '經典抹茶',
+          title: '抹茶千層蛋糕',
+          price: 500,
+          img: './images/抹茶千層260X260.jpg'
+        },
+        {
+          sort: '經典抹茶',
+          title: '抹茶毛巾捲',
+          price: 500,
+          img: './images/抹茶毛巾260X260.jpg'
+        },
+        {
+          sort: '經典抹茶',
+          title: '抹茶提拉米蘇盒',
+          price: 500,
+          img: './images/抹茶提拉米蘇260X260.jpg'
+        },
+        {
+          sort: '新鮮草莓',
+          title: '草莓派對',
+          price: 500,
+          img: './images/草莓派對260X260.jpg'
+        },
+        {
+          sort: '經典抹茶',
+          title: '抹茶大福',
+          price: 500,
+          img: './images/抹茶大福260X260.jpg'
+        },
+        {
+          sort: '經典抹茶',
+          title: '抹茶奶香餅',
+          price: 500,
+          img: './images/抹茶奶香餅260X260.jpg'
+        },
+        {
+          sort: '經典抹茶',
+          title: '抹茶夾心酥',
+          price: 500,
+          img: './images/抹茶夾心酥260X260.jpg'
+        },
+        {
+          sort: '濃郁乳酪',
+          title: '柑橘乳酪',
+          price: 500,
+          img: './images/柑橘乳酪260X260.jpg'
+        },
+        {
+          sort: '新鮮草莓',
+          title: '草莓奶油泡芙',
+          price: 500,
+          img: './images/草莓奶油泡芙260X260.jpg'
+        }
+      ],
+    }
+  },
+
+}).mount("#app")
